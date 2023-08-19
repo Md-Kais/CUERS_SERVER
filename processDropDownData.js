14c80133f8b96e6217595dc8a66359e861b30261
voidCounter <rohitroy7014@gmail.com>
2023-08-19 14:18:35 +0600

    Added some comments on processDropdown

--- processDropDownData.js
+++ processDropDownData.js
@@ -1,11 +1,11 @@
-const {loadData} = require('./loadData.js');
-const {conn} = require('./connectDatabase.js');
+const { loadData } = require('./loadData.js');
+const { conn } = require('./connectDatabase.js');
 const toEnglishNumber = require('./Modules/toEnglishNumber.js');
 const toBanglaNumber = require('./Modules/toBanglaNumber.js');
 
 const fs = require('fs');
 
-async function processDropDownData(props){
+async function processDropDownData(props) {
     const {
         dynamicOps,
         tableName,
@@ -14,151 +14,193 @@ async function processDropDownData(props){
         storageLabel,
         condition,
         value,
-      } = props;
+    } = props;
 
+    /*
+     * We're using two separate file for reading and writing dropdown options.
+     * backup file is used for safety reason. If there is any problem while
+     * writing at dropdown_options.json, it may get empty. So on the next
+     * reading we can read from the backup file and also write the contents of
+     * the backup file to the main file(dropdown_options.json).
+     */
     const filePath = './Data/dropdown_options.json';
     const backupFilePath = './Data/dropdown_options_backup.json';
     let existingData = fs.readFileSync(filePath, 'utf-8');
+    // jsonData contains the contents of the dropdown_options.json and backupData
+    // contains the contents of the dropdown_options_backup.json
     let jsonData, backupData;
     console.log('Existing data: ', existingData);
     try {
-      jsonData = JSON.parse(existingData);
+        jsonData = JSON.parse(existingData);
     } catch (err) {
-      console.log('jsonData is empty, trying to get backup');
-      if (jsonData == null || Object.keys(jsonData).length == 0) {
-        try {
-          existingData = fs.readFileSync(backupFilePath, 'utf-8');
-          jsonData = JSON.parse(existingData);
-          // make a copy of the backupData
-          backupData = JSON.parse(JSON.stringify(jsonData));
-        } catch (err) {
-          console.log('backup is also empty, trying to get {}');
-          if (jsonData == null || Object.keys(jsonData).length == 0) {
-            jsonData = {};
-          }
+        console.log('jsonData is empty, trying to get backup');
+        if (jsonData == null || Object.keys(jsonData).length == 0) {
+            try {
+                // if jsonData is empty, we're reading from the backup file and
+                // storing it into jsonData
+                existingData = fs.readFileSync(backupFilePath, 'utf-8');
+                jsonData = JSON.parse(existingData);
+                // make a copy of the backupData
+                backupData = JSON.parse(JSON.stringify(jsonData));
+            } catch (err) {
+                console.log('backup is also empty, trying to get {}');
+                // even if backupData is also empty, then we're initializing a
+                // new object to add new dropdown options from frontend.
+                if (jsonData == null || Object.keys(jsonData).length == 0) {
+                    jsonData = {};
+                }
+            }
         }
-      }
     }
 
     if (operation && operation == 'write') {
-      if (jsonData && !jsonData[storageLabel]) {
-        jsonData[storageLabel] = [value];
-      } else {
-        jsonData[storageLabel].push(value);
-      }
-      if (jsonData != null && Object.keys(jsonData).length != 0) {
-        fs.writeFile(
-          backupFilePath,
-          JSON.stringify(jsonData),
-          (backupError) => {
-            if (backupError) {
-              console.error(backupError);
-              return { error: 'Error updating data' };
-              // res.status(500).json({ error: 'Error updating data' });
-              // return;
-            }
-            fs.writeFile(filePath, JSON.stringify(jsonData), (fileError) => {
-              if (fileError) {
-                console.error(fileError);
-                return { error: 'Error updating data' };
-                // res.status(500).json({ error: 'Error updating data' });
-                // return;
-              }
-              return JSON.stringify(jsonData);
-              // res.json(JSON.stringify(jsonData));
-            });
-          }
-        );
-      } else {
-        return JSON.stringify(backupData)
-        // res.json(JSON.stringify(backupData));
-      }
-    } else {
-      if (dynamicOps == true) {
-        console.log('DynamicOps are true');
-        let colString = cols.join(', ');
-
-        let dataX = await processingDropDownData(tableName, colString, condition);
-        let result = dataX.map((item) => Object.values(item).join(' - '));
-        const dropdownOptions = result.reduce((acc, option) => {
-          const [id, name] = option.split(' - ');
-          // console.log('spliting: ', id, name);
-          const englishId = toEnglishNumber(id.trim().split(' ')[0]);
-          if (tableName != 'Course') {
-            acc[englishId] = `${toBanglaNumber(id)}- ${name}`;
-          } else {
-            acc[englishId] = `${toEnglishNumber(id)} - ${name}`;
-          }
-          return acc;
-        }, {});
-        // Update or add data to the JSON file
-        // Data already exists, update it
-        jsonData[storageLabel] = dropdownOptions;
-
-        console.log('DynamicOps are generated, now jsonData: ', jsonData);
-        // Write the updated data back to the JSON file
+        if (jsonData && !jsonData[storageLabel]) {
+            jsonData[storageLabel] = [value];
+        } else {
+            jsonData[storageLabel].push(value);
+        }
         if (jsonData != null && Object.keys(jsonData).length != 0) {
-          fs.writeFile(
-            backupFilePath,
-            JSON.stringify(jsonData),
-            (backupError) => {
-              if (backupError) {
-                console.error(backupError);
-                return { error: 'Error updating data' };
-                // res.status(500).json({ error: 'Error updating data' });
-                // return;
-              }
-              console.log('Inside writeFile!!!')
-              fs.writeFile(filePath, JSON.stringify(jsonData), (fileError) => {
-                if (fileError) {
-                  // console.error(fileError);
-                  // res.status(500).json({ error: 'Error updating data' });
-                  return { error: 'Error updating data' };
+            // if jsonData is not null, that means jsonData already contains
+            // options, we're writing the passed data to the both file
+            fs.writeFile(
+                backupFilePath,
+                JSON.stringify(jsonData),
+                (backupError) => {
+                    if (backupError) {
+                        console.error(backupError);
+                        return { error: 'Error updating data' };
+                        // res.status(500).json({ error: 'Error updating data' });
+                        // return;
+                    }
+                    fs.writeFile(
+                        filePath,
+                        JSON.stringify(jsonData),
+                        (fileError) => {
+                            if (fileError) {
+                                console.error(fileError);
+                                return { error: 'Error updating data' };
+                                // res.status(500).json({ error: 'Error updating data' });
+                                // return;
+                            }
+                            return JSON.stringify(jsonData);
+                            // res.json(JSON.stringify(jsonData));
+                        }
+                    );
                 }
-                return JSON.stringify(jsonData);
-              });
-            }
-          );
+            );
         } else {
-          return JSON.stringify(backupData);
-        }
-      } else {
-        if (jsonData != null && Object.keys(jsonData).length != 0) {
-          return JSON.stringify(jsonData);
-        } else {
-          fs.writeFile(filePath, JSON.stringify(jsonData), (fileError) => {
-            if (fileError) {
-              console.error(fileError);
-              // res.status(500).json({ error: 'Error updating data' });
-              // return;
-              return { error: 'Error updating data' };
-            }
-            // res.json(JSON.stringify(backupData));
             return JSON.stringify(backupData);
-          });
+            // res.json(JSON.stringify(backupData));
         }
-      }
+    } else {
+        // if we have to read the options
+        if (dynamicOps == true) {
+            // dynamicOps == true means we need to load the from table
+            console.log('DynamicOps are true');
+            let colString = cols.join(', ');
+
+            // processingDropDownData function is used to fetch option data from table
+            let dataX = await processingDropDownData(
+                tableName,
+                colString,
+                condition
+            );
+
+            // mapping the data to possible key
+            let result = dataX.map((item) => Object.values(item).join(' - '));
+            const dropdownOptions = result.reduce((acc, option) => {
+                const [id, name] = option.split(' - ');
+                // console.log('spliting: ', id, name);
+                const englishId = toEnglishNumber(id.trim().split(' ')[0]);
+                if (tableName != 'Course') {
+                    acc[englishId] = `${toBanglaNumber(id)}- ${name}`;
+                } else {
+                    acc[englishId] = `${toEnglishNumber(id)} - ${name}`;
+                }
+                return acc;
+            }, {});
+            jsonData[storageLabel] = dropdownOptions;
+
+            console.log('DynamicOps are generated, now jsonData: ', jsonData);
+
+            // Now we need to keep the jsonData or the json file updated. So
+            // after reading from the database, we're writing the data to the
+            // json files
+            if (jsonData != null && Object.keys(jsonData).length != 0) {
+                // writing in the backup file
+                fs.writeFile(
+                    backupFilePath,
+                    JSON.stringify(jsonData),
+                    (backupError) => {
+                        if (backupError) {
+                            console.error(backupError);
+                            return { error: 'Error updating data' };
+                            // res.status(500).json({ error: 'Error updating data' });
+                            // return;
+                        }
+                        console.log('Inside writeFile!!!');
+                        // And also writing in the main file
+                        fs.writeFile(
+                            filePath,
+                            JSON.stringify(jsonData),
+                            (fileError) => {
+                                if (fileError) {
+                                    // console.error(fileError);
+                                    // res.status(500).json({ error: 'Error updating data' });
+                                    return { error: 'Error updating data' };
+                                }
+                                return JSON.stringify(jsonData);
+                            }
+                        );
+                    }
+                );
+            } else {
+                return JSON.stringify(backupData);
+            }
+        } else {
+            // if we don't need to load them from the table
+            // we can just send them to frontend
+            if (jsonData != null && Object.keys(jsonData).length != 0) {
+                return JSON.stringify(jsonData);
+            } else {
+                // This part Needs review
+                fs.writeFile(
+                    filePath,
+                    JSON.stringify(jsonData),
+                    (fileError) => {
+                        if (fileError) {
+                            console.error(fileError);
+                            // res.status(500).json({ error: 'Error updating data' });
+                            // return;
+                            return { error: 'Error updating data' };
+                        }
+                        // res.json(JSON.stringify(backupData));
+                        return JSON.stringify(backupData);
+                    }
+                );
+            }
+        }
     }
     return jsonData;
 }
 
 async function processingDropDownData(tableName, colString, condition) {
     try {
-      let data = [];
-      let query = ``;
-      if (condition) {
-        query = `SELECT DISTINCT ${colString} FROM ${tableName} where ${condition}`;
-      } else {
-        query = `SELECT DISTINCT ${colString} FROM ${tableName}`;
-      }
-      console.log(query);
-      data = await loadData(conn, null, null, query);
-      //console.log("Loaded data: ", data);
-      return data;
+        let data = [];
+        let query = ``;
+        if (condition) {
+            query = `SELECT DISTINCT ${colString} FROM ${tableName} where ${condition}`;
+        } else {
+            query = `SELECT DISTINCT ${colString} FROM ${tableName}`;
+        }
+        console.log(query);
+        data = await loadData(conn, null, null, query);
+        //console.log("Loaded data: ", data);
+        return data;
     } catch (err) {
-      console.error(err);
-      throw new Error('Error loading data');
+        console.error(err);
+        throw new Error('Error loading data');
     }
-  }
+}
 
-  module.exports = {processDropDownData}
+module.exports = { processDropDownData };
--- users.js
+++ users.js
@@ -5,13 +5,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 router.use(cors());
 const {
-  getActivityList,
-  getCourseActivityTable,
-  getSemesterActivityTable,
+    getActivityList,
+    getCourseActivityTable,
+    getSemesterActivityTable,
 } = require('./pdfGeneration.js');
 const { getBill } = require('./billGeneration.js');
-const {processData} = require('./processData.js');
-const {processDropDownData} = require('./processDropDownData.js')
+const { processData } = require('./processData.js');
+const { processDropDownData } = require('./processDropDownData.js');
 
 router.use(bodyParser.json());
 
@@ -19,186 +19,190 @@ router.use(bodyParser.json());
 const { conn } = require('./connectDatabase.js');
 
 router.use((req, res, next) => {
-  console.log('Request made to /USERS Route');
-  next(); //needs to go to the middleware
+    console.log('Request made to /USERS Route');
+    next(); //needs to go to the middleware
 });
 
 router.get('/posts', (req, res) => {
-  res.json({ route: 'Posts' });
+    res.json({ route: 'Posts' });
 });
 
 // Loading for the first time is not working {}
 router.post('/loadTableInfo', (req, res) => {
-  const { tableNames } = req.body;
-  let tableInfo = {};
-  let tableDesc = {};
-  const promises = tableNames.map((tableName) => {
-    return new Promise((resolve, reject) => {
-      const query = `desc ${tableName}`;
-      conn.query(query, (err, results) => {
-        if (err) {
-          reject(err);
-        } else {
-          tableDesc[tableName] = results;
-          resolve();
-        }
-      });
+    const { tableNames } = req.body;
+    let tableInfo = {};
+    let tableDesc = {};
+    const promises = tableNames.map((tableName) => {
+        return new Promise((resolve, reject) => {
+            const query = `desc ${tableName}`;
+            conn.query(query, (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    tableDesc[tableName] = results;
+                    resolve();
+                }
+            });
+        });
     });
-  });
 
-  Promise.all(promises)
-    .then(() => {
-      // getting the primary keys of each
-      const tableInfo = {};
-      for (table in tableDesc) {
-        const tempInfo = {};
-        let primaryKeys = [];
-        let dataTypes = {};
-        primaryKeys = tableDesc[table]
-          .filter((item) => item.Key === 'PRI')
-          .map((item) => item.Field);
-        tempInfo['primaryKeys'] = primaryKeys;
-        // getting the types;
-        dataTypes = tableDesc[table].reduce((result, { Field, Type }) => {
-          result[Field] = Type;
-          // console.log("The result is:", result);
-          return result;
-        }, {});
-        tempInfo['dataTypes'] = dataTypes;
-        tableInfo[table] = tempInfo;
-      }
-      res.json(tableInfo);
-    })
-    .catch((err) => {
-      console.error('Error getting table desc: ', err);
-      res.status(500).send('Internal server Error');
-    });
+    Promise.all(promises)
+        .then(() => {
+            // getting the primary keys of each
+            const tableInfo = {};
+            for (table in tableDesc) {
+                const tempInfo = {};
+                let primaryKeys = [];
+                let dataTypes = {};
+                primaryKeys = tableDesc[table]
+                    .filter((item) => item.Key === 'PRI')
+                    .map((item) => item.Field);
+                tempInfo['primaryKeys'] = primaryKeys;
+                // getting the types;
+                dataTypes = tableDesc[table].reduce(
+                    (result, { Field, Type }) => {
+                        result[Field] = Type;
+                        // console.log("The result is:", result);
+                        return result;
+                    },
+                    {}
+                );
+                tempInfo['dataTypes'] = dataTypes;
+                tableInfo[table] = tempInfo;
+            }
+            res.json(tableInfo);
+        })
+        .catch((err) => {
+            console.error('Error getting table desc: ', err);
+            res.status(500).send('Internal server Error');
+        });
 });
 
 router.post('/processDropDownData', async (req, res) => {
-  processDropDownData(req.body.data.params).then((result) =>{
-    // console.log("Besult is here:", result)
-    res.json(JSON.stringify(result))
-    res.end()
-  }).catch((err) =>{
-    console.log(err)
-    res.status(400).send(err);
-  })
+    processDropDownData(req.body.data.params)
+        .then((result) => {
+            // console.log("Besult is here:", result)
+            res.json(JSON.stringify(result));
+            res.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).send(err);
+        });
 });
 
 router.post('/processData', (req, res) => {
-  const { changes, getTableInfo, query } = req.body;
-  processData(conn, changes, getTableInfo, query)
-    .then((data) => {
-      // console.log("Query status", data);
-      res.json(data);
-      res.end();
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
+    const { changes, getTableInfo, query } = req.body;
+    processData(conn, changes, getTableInfo, query)
+        .then((data) => {
+            // console.log("Query status", data);
+            res.json(data);
+            res.end();
+        })
+        .catch((error) => {
+            res.status(400).send(error);
+        });
 });
 
-router.post('/process_semester_info',(req,res) => {
-
-  const {query}=req.body;
-  //console.log(query);
-    conn.query(query,function(error,data){
-      if(data){
-        res.json(JSON.stringify(data));  
-        // console.log(data);
-      }
-    })
+router.post('/process_semester_info', (req, res) => {
+    const { query } = req.body;
+    //console.log(query);
+    conn.query(query, function (error, data) {
+        if (data) {
+            res.json(JSON.stringify(data));
+            // console.log(data);
+        }
+    });
 });
 
 router.post('/authenticatelogin', (req, res) => {
-  const { evaluator_id, password, role } = req.body;
-  console.log(evaluator_id, password, role);
-  if (evaluator_id && password) {
-    //console.log(evaluator_id + role);
-    const query = `
+    const { evaluator_id, password, role } = req.body;
+    console.log(evaluator_id, password, role);
+    if (evaluator_id && password) {
+        //console.log(evaluator_id + role);
+        const query = `
         select * from Login_Info
         where evaluator_id = "${evaluator_id}" and role = "${role}"
         `;
-    conn.query(query, function (error, data) {
-      console.log(data);
-      if (data?.length > 0) {
-        for (var count = 0; count < data.length; count++) {
-          if (
-            data[count].password === password &&
-            data[count].role.localeCompare(role) == 0
-          ) {
-            res.status(200);
-            return res.json({ msg: 'Correct Password' });
-          } else if (data[count].password == password) {
-            return res.json({ msg: 'Incorrect Role' });
-          } else {
-            return res.json({ msg: 'Incorrect Password' });
-          }
-          console.log(res);
-        }
-      } else {
-        return res.json({ msg: 'Incorrect Evaluator Id' });
-      }
-      res.end();
-    });
-  }
+        conn.query(query, function (error, data) {
+            console.log(data);
+            if (data?.length > 0) {
+                for (var count = 0; count < data.length; count++) {
+                    if (
+                        data[count].password === password &&
+                        data[count].role.localeCompare(role) == 0
+                    ) {
+                        res.status(200);
+                        return res.json({ msg: 'Correct Password' });
+                    } else if (data[count].password == password) {
+                        return res.json({ msg: 'Incorrect Role' });
+                    } else {
+                        return res.json({ msg: 'Incorrect Password' });
+                    }
+                    console.log(res);
+                }
+            } else {
+                return res.json({ msg: 'Incorrect Evaluator Id' });
+            }
+            res.end();
+        });
+    }
 });
 
 router.post('/pdfGeneration', (req, res) => {
-  const {
-    semester_no,
-    evaluator_id,
-    to_get,
-    activity_type_id,
-    sector_or_program,
-    // year
-  } = req.body;
-  console.log(req.body);
-  if (to_get === 'activity_list') {
-    getActivityList(conn, semester_no).then((data) => {
-      res.json(data);
-      res.end();
-    });
-  } else if (to_get === 'courseActivities') {
-    getCourseActivityTable(
-      conn,
-      activity_type_id,
-      sector_or_program,
-      semester_no
-    ).then((data) => {
-      console.log('At getcourseActiivty: ', data);
-      res.json(data);
-      res.end();
-    });
-  } else {
-    getSemesterActivityTable(
-      conn,
-      activity_type_id,
-      sector_or_program,
-      semester_no
-    ).then((data) => {
-      res.json(data);
-      res.end();
-    });
-  }
+    const {
+        semester_no,
+        evaluator_id,
+        to_get,
+        activity_type_id,
+        sector_or_program,
+        // year
+    } = req.body;
+    console.log(req.body);
+    if (to_get === 'activity_list') {
+        getActivityList(conn, semester_no).then((data) => {
+            res.json(data);
+            res.end();
+        });
+    } else if (to_get === 'courseActivities') {
+        getCourseActivityTable(
+            conn,
+            activity_type_id,
+            sector_or_program,
+            semester_no
+        ).then((data) => {
+            console.log('At getcourseActiivty: ', data);
+            res.json(data);
+            res.end();
+        });
+    } else {
+        getSemesterActivityTable(
+            conn,
+            activity_type_id,
+            sector_or_program,
+            semester_no
+        ).then((data) => {
+            res.json(data);
+            res.end();
+        });
+    }
 });
 
 router.post('/activityBillData', (req, res) => {
-  getBill(conn, req.body).then((data) => {
-    console.log('Data inside router', data);
-    res.json(data);
-    res.end();
-  });
+    getBill(conn, req.body).then((data) => {
+        console.log('Data inside router', data);
+        res.json(data);
+        res.end();
+    });
 });
 
 router.get('/', (req, res) => {
-  conn.query('SELECT * from Login_Info', function (err, rows, fields) {
-    if (err) throw err;
-    console.log(rows);
-    res.json({ rows });
-    //   res.send(`Result: ${rows[0][]}`);
-  });
+    conn.query('SELECT * from Login_Info', function (err, rows, fields) {
+        if (err) throw err;
+        console.log(rows);
+        res.json({ rows });
+        //   res.send(`Result: ${rows[0][]}`);
+    });
 });
 
 module.exports = router;
